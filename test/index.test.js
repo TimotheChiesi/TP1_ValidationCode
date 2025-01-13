29c153f964fbcb2acf140a42e3842a1d34fcd95f
Timothe C <timothe.chiesi@etu.mines-ales.fr>
2025-01-13 15:19:27 +0100

    point 2

---
+++ test/index.test.js
@@ -0,0 +1,32 @@
+import {my_alpha_number_t, sum, my_size_alpha_t} from '../src/';
+
+
+describe('my_alpha_number_t', () => {
+  it('Je souhaite tester si la fonction my_alpha_number_t renvoi bien “Hello”', () => {
+    expect(my_alpha_number_t("Hello")).toBe('Hello');
+  });
+  it('Je souhaite passer un argument “bonjour” le retour attendu est “Bonjour”', () => {
+    expect(my_alpha_number_t("Bonjour")).toBe('Bonjour');
+  });
+  it('Je souhaite passer un argument avec la valeur “” (vide) le retour attendu sera “Hello”', () => {
+    expect(my_alpha_number_t("")).toBe('');
+  });
+});
+
+describe('sum', () => {
+    it('Should return the sum of 2+2; which is 4', () => {
+        expect(sum(2, 2)).toBe(4);
+    });      
+    it('Should return the sum of "2"+"2"; which is 0', () => {
+        expect(sum('2', '2')).toBe(0);
+    });
+});
+
+describe('my_size_alpha_t', () => {
+    it('Should return the length of the string "hello world" which is 11', () => {
+        expect(my_size_alpha_t('hello world')).toBe(11);
+    });
+    it('Should return 0 for non-string inputs', () => {
+    expect(my_size_alpha_t(12345)).toBe(0);
+    });
+});
